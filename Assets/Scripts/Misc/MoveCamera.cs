9b497d08c930e2d158d40b7415b71b7432f47cc8
WesleyVong <wv@vongs.com>
2021-04-25 15:24:59 -0700

    Update MoveCamera.cs

--- Assets/Scripts/Misc/MoveCamera.cs
+++ Assets/Scripts/Misc/MoveCamera.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
+    float startTime;
+    float elapsed = 0;
+    float travelTime = 0;
+    Vector2 initPos;
+    Vector2 destPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveToPos(new Vector3(0, -10, -10), 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        elapsed = Time.time - startTime;
+
+    }
+
+    public void MoveToPos(Vector3 pos, float time)
+    {
+        initPos = transform.position;
+        startTime = Time.time;
+        travelTime = time;
     }
 }
